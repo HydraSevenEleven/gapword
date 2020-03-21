13e2d2083a7f04e2b7f2e079d9572905d67fbc75
velopers-de <daliacode@gmail.com>
2020-03-21 18:26:04 +0100

    structure changed

--- quizloader.py
+++ quizloader.py
@@ -1,5 +1,6 @@
 import random
-from enum import Enum
+import definitions
+import definition_handler
 
 class QuizLoader:
 
@@ -8,32 +9,23 @@ class QuizLoader:
 		self.num_sentences = num_sentences
 
 	def get_sentences(self):
+		""" returns a list with the sentences from the defined database file """
 		file = open(self.db_file).readlines()
-		content = [x.strip('\n') for x in file]
+		content = [x.strip("\n\t") for x in file]
 		return content
 
 	def get_random_sentences(self):
-		list = self.get_sentences()
-		list_of_random_sentences = random.sample(list,self.num_sentences)
-		print(list_of_random_sentences)
+		""" get random sentences from the database as a list """	
+		return random.sample(self.get_sentences(),self.num_sentences)
 
-	def get_word_definition(self,word):
-		for definition in (Definitions):
-			if word in set(definition.value):
-				return definition
 
-	def get_elements_from_word_definitions(self,word_def):
-		print(random.sample(word_def.value,3))
-		print(word_def)
-
-class Definitions(Enum):
-	DEFINED_ARTICLE		= ("der","die","das","den","dem")
-	UNDEFINED_ARTICLE 	= ("ein","einen","eine","einem","einer","eines")
 
 if __name__ == "__main__":
 	x = QuizLoader("database",2)
-	x.get_random_sentences()	
-	wd = x.get_word_definition("der")
+	print(x.get_random_sentences())	
+	
+	y = definition_handler.DefinitionsHandler() 	
+	wd = y.get_word_definition("der")
 	print(wd)
-	x.get_elements_from_word_definitions(wd)
+	y.get_elements_from_word_definitions(wd)
 	
