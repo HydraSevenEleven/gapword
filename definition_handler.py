9dad7bc5a1fb19681615c468c0a017b3ff5089b2
velopers-de <daliacode@gmail.com>
2020-03-22 10:26:32 +0100

    refactoring

--- definition_handler.py
+++ definition_handler.py
@@ -3,12 +3,20 @@ import definitions
 
 class DefinitionsHandler:
 
-	def get_word_definition(self,word):
+	def get_set_possibly_answers(self,word):
 		""" looking for the definition of a given word under the Definitions enum class """
 		for definition in (definitions.Definitions):
 			if word in set(definition.value):
 				return definition
 
-	def get_elements_from_word_definitions(self,word_def):
+	def get_multiple_choice(self,right_answer):
 		""" returns 3 random sample from definition list """
-		return random.sample(word_def.value,3)
+		possibly_answers = self.get_set_possibly_answers(right_answer)
+		# the right answer should not be twice in the list 
+		list_possibly_answer = list(possibly_answers.value)
+		list_possibly_answer.remove(right_answer)
+		false_answers = random.sample(list_possibly_answer,3)
+		false_answers.append(right_answer)
+		multiple_choice = random.sample(false_answers,4)
+		return multiple_choice
+			
--- quizloader.py
+++ quizloader.py
@@ -1,12 +1,14 @@
 import random
-import definitions
-import definition_handler
+import json
+from definition_handler import DefinitionsHandler as def_hdl
+
 
 class QuizLoader:
 
 	def __init__(self,db_file,num_sentences):
 		self.db_file = db_file
 		self.num_sentences = num_sentences
+		self.hdl = def_hdl()
 
 	def get_sentences(self):
 		""" returns a list with the sentences from the defined database file """
@@ -17,15 +19,21 @@ class QuizLoader:
 	def get_random_sentences(self):
 		""" get random sentences from the database as a list """	
 		return random.sample(self.get_sentences(),self.num_sentences)
-
-
+	
+	def set_questions(self):
+		""" set the questions """
+		my_json_string=[]
+		list_of_random_sentences = self.get_random_sentences()
+		for sentence in list_of_random_sentences:
+			splitted_sentence = sentence.split('|')
+			possibly_answer_list = self.hdl.get_multiple_choice(splitted_sentence[1])
+			item = {'sentence':splitted_sentence[0],'choice':possibly_answer_list}
+			my_json_string.append(item)
+		jsonData = json.dumps(my_json_string, ensure_ascii=False)
+		print(jsonData)
 
 if __name__ == "__main__":
 	x = QuizLoader("database",2)
-	print(x.get_random_sentences())	
+	x.set_questions()	
 	
-	y = definition_handler.DefinitionsHandler() 	
-	wd = y.get_word_definition("der")
-	print(wd)
-	y.get_elements_from_word_definitions(wd)
 	
